82933fd94dda2853e0b8472af3d89982c8ca4b1c
PuskarMal <puskarmal07@gmail.com>
2025-07-02 16:27:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const connectToMongo = async () => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/");
+        await mongoose.connect("mongodb+srv://puskar07:GOeXpgBp4Fi9p3bO@cluster0.mongodb.net/test",{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
         console.log("Connected successfully to database")
     } catch (error) {
         console.log(error)
